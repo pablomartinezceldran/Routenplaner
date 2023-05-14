dabf6fbf488485c3c95d52f66d0a4415c357b9f7
st178931 <st178931@stud.uni-stuttgart.de>
2023-05-14 20:19:40 +0200

    Habe diesen Satz übersehen: "nodeID2, elevation, type and maxspeed can be ignored for now" und diese Variablen deshalb nun entfernt.

--- src/main/java/models/Edge.java
+++ src/main/java/models/Edge.java
@@ -4,15 +4,11 @@ public class Edge {
     int scrIDX;
     int trgIDX;
     int cost;
-    int type;
-    int maxspeed;
 
 
-    public Edge(int scrIDX, int trgIDX, int cost, int type, int maxspeed){
+    public Edge(int scrIDX, int trgIDX, int cost){
         this.scrIDX=scrIDX;
         this.trgIDX=trgIDX;
         this.cost=cost;
-        this.type=type;
-        this.maxspeed=maxspeed;
     }
 }
--- src/main/java/models/Node.java
+++ src/main/java/models/Node.java
@@ -2,23 +2,18 @@ package main.java.models;
 
 public class Node {
     int nodeID;
-    int nodeID2;
 
     /*x-Achse*/
     double latitude;
 
     /*y-Achse*/
     double longitude;
-    int elevation;
 
 
-    public Node(int nodeID, int nodeID2, double latitude, double longitude, int elevation){
+    public Node(int nodeID, double latitude, double longitude){
         this.nodeID=nodeID;
-        this.nodeID2=nodeID2;
         this.latitude=latitude;
         this.longitude=longitude;
-        this.elevation=elevation;
-
     }
 
 }
