a9bc6f1da526124383744698da783ffb531fae90
pablomartinezceldran <pablo.martinez.celdran@estudiantat.upc.edu>
2023-06-04 21:44:14 +0200

    used new map graph, previous one didnt work properly. memory consumption shown (can be deleted)

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,21 @@
+import main.java.models.MapGraph1;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        // can be deleted
+        System.out.println("-----------------------------------------");
+        long memoryBefore = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        MapGraph1 map = new MapGraph1();
+        map.fillMap();
+
+        // can be deleted
+        long memoryAfter = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        long memoryUsed = memoryAfter - memoryBefore;
+        System.out.println("Memory consumption: " + memoryUsed + " bytes");
+        System.out.println("-----------------------------------------");
     }
 }
