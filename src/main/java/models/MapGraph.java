c270bc8eda9c8d6844afb8c2a70af242e258f437
st178931 <st178931@stud.uni-stuttgart.de>
2023-06-12 19:02:17 +0200

    removed extra class

--- src/main/java/models/MapGraph.java
+++ src/main/java/models/MapGraph.java
@@ -55,53 +55,7 @@ public class MapGraph {
 
 }
 */
-public class MapGraph {
-    /*unvollständig*/
-    //ArrayList<ArrayList<Double>> adjList = new ArrayList<ArrayList<Double>>();
-    double[] OffsetMap = new double[0];
-    double[][] EdgeMap = new double[0][0];
-    double nodeID1Map[] = new double[0];
 
-    public void createGraphs(String datName) {
-        /*File file = new File(datName);*/
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(datName));
-            for(int i=0;i<5;i++) in.readLine();
-            int numNodes= in.read();
-            int numEdges= in.read();
-            for(int i=0;i<numNodes;i++){
-                in.readLine();
-            }
-            //Fülle die Kanten liste (siehe Intro Phase I)
-            for(int i=0;i<numEdges;i++) {
-                double offset = 0;
-                String edge = in.readLine();
-                String[] split = edge.split(" "); // [srcIDX, trgIDX, cost, type, maxspeed]
-                double nodeID1 = Double.parseDouble(split[0]);
-                double nodeID2 = Double.parseDouble(split[0]);
-                double cost = Double.parseDouble(split[0]);
-                EdgeMap[i][0] = nodeID1;
-                EdgeMap[i][1] = nodeID2;
-                EdgeMap[i][2] = cost;
-                nodeID1Map[i] = nodeID1;
-            }
-            //Fülle die Offset liste (siehe Intro Phase I)
-            double maxID=0;
-            for(double nodeID: nodeID1Map){
-                if(nodeID>maxID) maxID++;
-                else OffsetMap[(int) maxID]+=1;
-            }
-
-        }catch(FileNotFoundException exc){
-            System.out.println("Invalid File");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-
-}
 public class MapGraph {
 
     private final ArrayList<ArrayList<Edge>> adjList = new ArrayList<>();
