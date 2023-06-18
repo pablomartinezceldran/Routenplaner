c36a95fc5190331a03e6e67715dca1f28f68465c
pablomartinezceldran <pablo.martinez.celdran@estudiantat.upc.edu>
2023-06-18 13:15:58 +0200

    checking closest node algorithm

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,7 @@
 package main.java;
 
-import main.java.models.MapGraph;
+import main.java.models.map.MapGraph;
+import main.java.models.map.Node;
 
 import java.io.IOException;
 
@@ -19,10 +20,43 @@ public class Application {
         //map.createGraph("resources/germany.fmi");
 
 
+        System.out.println("-----------------------------------------");
+
+//        System.out.println("Dijkstra algorithm:");
+//        long startDijkstra = System.currentTimeMillis();
+//        int sourceNode = 1;
+//
+//
+//        List<Integer> path = new DijkstraAlgorithm().findShortestPath(map, 1, 2);
+//
+//        long endDijkstra = System.currentTimeMillis();
+//
+//        for (int i = 0; i < path.size(); ++i) {
+//            if (i % 1000000 == 0) {
+//                System.out.println("Distance from node: " + sourceNode + " to node: " + i + " is " + path.get(i));
+//            }
+//        }
+//
+//        System.out.println("-----------------------------------------");
+//        System.out.println("Dijkstra time: " + (endDijkstra - startDijkstra) + " miliseconds");
+
+        System.out.println("ClosestNode algorithm:");
+        long startFindClosestNode = System.currentTimeMillis();
+
+//        KDTree kdTree = new KDTree(map.getNodeList());
+        long treeTime = System.currentTimeMillis();
+        Node nearestNode = map.getNodeTree().findNearestNode(5024428900000000198L, 700798490000000029L);
+//        Node nearestNode = kdTree.findNearestNode(4864209470000000124L, 901481120000000047L);
+
+        long endFindClosestNode = System.currentTimeMillis();
+        System.out.println("Tree creation time: " + (treeTime - startFindClosestNode) + " miliseconds");
+        System.out.println("ClosestNode time: " + (endFindClosestNode - treeTime) + " miliseconds");
+        System.out.println("Closest node: " + nearestNode.getNodeID());
+
         // can be deleted
+        System.out.println("-----------------------------------------");
         long memoryAfter = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
         long memoryUsed = memoryAfter - memoryBefore;
         System.out.println("Memory consumption: " + memoryUsed + " bytes");
-        System.out.println("-----------------------------------------");
     }
 }
