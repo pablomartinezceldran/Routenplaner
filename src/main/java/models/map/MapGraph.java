984ea54e27f06fbf198d7dc6dec831e7e650ed87
pablomartinezceldran <pablo.martinez.celdran@estudiantat.upc.edu>
2023-06-19 16:55:16 +0200

    removed commented and unused code

--- src/main/java/models/map/MapGraph.java
+++ src/main/java/models/map/MapGraph.java
@@ -1,7 +1,5 @@
 package main.java.models.map;
 
-import main.java.models.KDTree.KDTree;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,15 +10,10 @@ public class MapGraph {
     private final ArrayList<ArrayList<Edge>> adjList = new ArrayList<>();
     private final ArrayList<Node> nodeList = new ArrayList<>();
 
-    private final KDTree nodeTree = new KDTree();
-
     private Integer numNodes;
     private Integer numEdges;
 
     public void fillMap(String graphPath) {
-        // select fmi file
-//        BufferedReader in = new BufferedReader(new FileReader("resources/toy.fmi"));
-//        BufferedReader in = new BufferedReader(new FileReader("resources/stgtregbz.fmi"));
         try {
             BufferedReader in = new BufferedReader(new FileReader(graphPath));
 
@@ -32,10 +25,7 @@ public class MapGraph {
             numNodes = Integer.parseInt(in.readLine());
             numEdges = Integer.parseInt(in.readLine());
 
-            // check time to read nodes and edges. can be deleted
-//            long nodesReadTime = System.currentTimeMillis();
-
-            //read nodes information. Variables are declared outside the loop to reduce memory usage
+            //read nodes information
             String[] values;
             int nodeId;
             Node node;
@@ -44,6 +34,8 @@ public class MapGraph {
                 values = in.readLine().split(" ");
                 nodeId = Integer.parseInt(values[0]);
 
+                // in order to store latitude and longitude without losing information
+                // and consuming as little memory as possible, we have opted for a long
                 node = new Node(
                         nodeId,
                         Long.parseLong(values[2].replace(".","")),
@@ -54,13 +46,10 @@ public class MapGraph {
 
                 // add node to KDTree. it is more efficient to create it this way instead of creating it after,
                 // but for the benchmark we need to create it after. Maybe we can change it fot next phase
-//                nodeTree.addNode(node);
+//              nodeTree.addNode(node);
             }
 
-            // check time to read nodes and edges. can be deleted
-//            long edgesReadTime = System.currentTimeMillis();
-
-            // read edges information. Variables are declared outside the loop to reduce memory usage
+            // read edges information
             int src;
             int trg;
             int cost;
@@ -74,48 +63,16 @@ public class MapGraph {
                 adjList.get(src).add(edge);
             }
             in.close();
-
-            // check time to read nodes and edges. can be deleted
-//            long endTime = System.currentTimeMillis();
-//            long nodesTime = edgesReadTime - nodesReadTime;
-//            long edgesTime = endTime - edgesReadTime;
-//            System.out.println("Nodes time: " + nodesTime + " miliseconds");
-//            System.out.println("Edges time: " + edgesTime + " miliseconds");
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    // prints node list. do not use with big maps, too many prints
-    public void printNodeList() {
-        for (Node node : nodeList) {
-            System.out.println(node.getNodeID() + " " + node.getLatitude() + " " + node.getLongitude());
-        }
-    }
-
-    // prints adj list. do not use with big maps, too many prints
-    public void printAdjList() {
-        for (int i = 0; i < adjList.size(); ++i) {
-            System.out.print(i + ": ");
-            for (Edge edge : adjList.get(i)) {
-                System.out.print(edge.getTarget() + "(" + edge.getCost() + ") ");
-            }
-            System.out.println();
-        }
-    }
-
-    public ArrayList<ArrayList<Edge>> getAdjList() {
-        return adjList;
-    }
-
-    public Integer getNumEdges() {
-        return numEdges;
     }
 
     public Integer getNumNodes() {
         return numNodes;
     }
 
+    //returns the edges of a node
     public ArrayList<Edge> getAdjacentEdges(int node) {
         return adjList.get(node);
     }
@@ -123,55 +80,4 @@ public class MapGraph {
     public ArrayList<Node> getNodeList() {
         return nodeList;
     }
-
-    public KDTree getNodeTree() {
-        return nodeTree;
-    }
 }
-
-/* I would implement it like this:
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class RoutePlanner {
-    private int[][] graph;
-    public RoutePlanner(int[][] graph) {
-        this.graph = graph;
-    }
-
-    //dijkstra algorithm
-    public int[] dijkstra(int startNode, int endNode) {
-        int[] distances = new int[graph.length];
-        boolean[] visited = new boolean[graph.length];
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-
-        Arrays.fill(distances, Integer.MAX_VALUE);
-        distances[startNode] = 0;
-
-        queue.add(new Node(startNode, 0));
-
-        while (!queue.isEmpty()) {
-            Node currentNode = queue.poll();
-            if (visited[currentNode.node]) {
-                continue;
-            }
-            visited[currentNode.node] = true;
-
-            for (int i = 0; i < graph[currentNode.node].length; i++) {
-                if (visited[i]) {
-                    continue;
-                }
-
-                int distance = currentNode.distance + graph[currentNode.node][i];
-
-                if (distance < distances[i]) {
-                    distances[i] = distance;
-                    queue.add(new Node(i, distance));
-                }
-            }
-        }
-        return distances[endNode];
-    }
- */
