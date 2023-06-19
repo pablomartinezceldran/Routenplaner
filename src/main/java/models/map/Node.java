4c4f6a5ba4bd27fac7c7b6030972a2f0e725b7c0
pablomartinezceldran <pablo.martinez.celdran@estudiantat.upc.edu>
2023-06-19 16:54:52 +0200

    removed commented code

--- src/main/java/models/map/Node.java
+++ src/main/java/models/map/Node.java
@@ -3,9 +3,7 @@ package main.java.models.map;
 public class Node {
 
     private final int nodeID;
-    /*x-Achse*/
     private final long latitude;
-    /*y-Achse*/
     private final long longitude;
 
 
@@ -27,50 +25,3 @@ public class Node {
         return longitude;
     }
 }
-/* I would implement it like this:
-
-//auxiliary class for dijkstra algorithm
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-private class Node implements Comparable<Node> {
-    int node;
-    int distance;
-
-    public Node(int node, int distance) {
-        this.node = node;
-        this.distance = distance;
-    }
-
-    @Override
-    public int compareTo(Node other) {
-        return Integer.compare(this.distance, other.distance);
-    }
-}
-
-    public int findNearestNode(double lon, double lat) {
-        double[] distances = new double[graph.length];
-        for (int i = 0; i < graph.length; i++) {
-            //Compute distance to node
-            double distance = Math.sqrt(Math.pow(lon - graph[i][0], 2) + Math.pow(lat - graph[i][1], 2));
-            distances[i] = distance;
-        }
-
-        // Find node with smallest distance
-        int minIndex = 0;
-        double minDistance = Double.MAX_VALUE;
-        for (int i = 0; i < distances.length; i++) {
-            if (distances[i] < minDistance) {
-                minIndex = i;
-                minDistance = distances[i];
-            }
-        }
-
-        //Return node with smallest distance
-        return minIndex;
-    }
-}
-
- */
