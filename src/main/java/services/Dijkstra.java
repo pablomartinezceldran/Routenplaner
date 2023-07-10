7f2a2fa9392646fd05fb0ff00c9e6d92fd1b83ca
pablomartinezceldran <pablo.martinez.celdran@estudiantat.upc.edu>
2023-07-10 22:55:08 +0200

    distance also saved in pq Edge and Node changed

--- src/main/java/services/Dijkstra.java
+++ src/main/java/services/Dijkstra.java
@@ -1,6 +1,5 @@
 package main.java.services;
 
-import main.java.models.map.Edge;
 import main.java.models.map.MapGraph;
 
 import java.util.ArrayList;
@@ -20,15 +19,16 @@ public class Dijkstra {
         // initialization queue
         // Comparator is used to specify the comparison criteria between the elements in the queue.
         // In our case, it compares the distances of the distances array.
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(nodeID -> distances[nodeID]));
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(node -> node[1]));
 
         // add source node to queue and set its distance to 0
-        queue.add(sourceID);
+        queue.add(new int[]{sourceID, 0});
         distances[sourceID] = 0;
 
         while (!queue.isEmpty()) {
-            int currentNodeID = queue.poll();
-            int currentDistance = distances[currentNodeID];
+            int[] currentNode = queue.poll();
+            int currentNodeID = currentNode[0];
+            int currentDistance = currentNode[1];
 
             // target reached, then return current distance
             if (currentNodeID == targetID) return currentDistance;
@@ -37,20 +37,21 @@ public class Dijkstra {
             if (currentDistance > distances[currentNodeID]) continue;
 
             // get neighbours of current node
-            ArrayList<Edge> neighbors = mapGraph.getAdjacentEdges(currentNodeID);
-            for (Edge neighbor : neighbors) {
-                int neighborID = neighbor.getTarget();
-                int newDistance = currentDistance + neighbor.getCost();
+            ArrayList<int[]> neighbors = mapGraph.getAdjacentEdges(currentNodeID);
+            for (int[] neighbor : neighbors) {
+                int neighborID = neighbor[0];
+                int newDistance = currentDistance + neighbor[1];
 
                 // update distance if shorter path to the neighbour is found
                 if (newDistance < distances[neighborID]) {
                     distances[neighborID] = newDistance;
-                    queue.add(neighborID);
+                    queue.add(new int[]{neighborID, newDistance});
                 }
             }
         }
         return -1;
     }
+
 
     // returns an array with distances from an existing source node to all nodes in the graph
     public static int[] oneToAll(MapGraph mapGraph, int sourceID) {
@@ -60,29 +61,30 @@ public class Dijkstra {
         Arrays.fill(distances, Integer.MAX_VALUE);
 
         // initialization queue. Comparator same as above
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(nodeID -> distances[nodeID]));
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(node -> node[1]));
 
         // add source node to queue and set its distance to 0
-        queue.add(sourceID);
+        queue.add(new int[]{sourceID, 0});
         distances[sourceID] = 0;
 
         while (!queue.isEmpty()) {
-            int currentNodeID = queue.poll();
-            int currentDistance = distances[currentNodeID];
+            int[] currentNode = queue.poll();
+            int currentNodeID = currentNode[0];
+            int currentDistance = currentNode[1];
 
             // current distance greater than stored distance, move on to next node
             if (currentDistance > distances[currentNodeID]) continue;
 
             // get neighbours of current node
-            ArrayList<Edge> neighbors = mapGraph.getAdjacentEdges(currentNodeID);
-            for (Edge neighbor : neighbors) {
-                int neighborID = neighbor.getTarget();
-                int newDistance = currentDistance + neighbor.getCost();
+            ArrayList<int[]> neighbors = mapGraph.getAdjacentEdges(currentNodeID);
+            for (int[] neighbor : neighbors) {
+                int neighborID = neighbor[0];
+                int newDistance = currentDistance + neighbor[1];
 
                 // update distance if shorter path to the neighbour is found
                 if (newDistance < distances[neighborID]) {
                     distances[neighborID] = newDistance;
-                    queue.add(neighborID);
+                    queue.add(new int[]{neighborID, newDistance});
                 }
             }
         }
