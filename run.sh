eef7427e0c209d97c8ca999063218c4d2dc3512e
pablomartinezceldran <pablo.martinez.celdran@estudiantat.upc.edu>
2023-08-03 21:04:56 +0200

    project runner

---
+++ run.sh
@@ -0,0 +1,37 @@
+# This script compiles and runs the server and opens the index.html in firefox or chrome.
+echo "Compiling server..."
+javac -d out src/main/java/Server.java src/main/java/services/Dijkstra.java src/main/java/models/k2Tree/K2Tree.java src/main/java/models/k2Tree/K2Node.java src/main/java/models/map/MapGraph.java
+echo "Done!"
+
+echo "Running server..."
+java -Xmx8G -Dfile.encoding=UTF-8 -classpath out main.java.Server &
+sleep 120
+echo "Done!"
+
+echo "Opening web client..."
+# open index.html &
+# echo "Done!"
+# Check the operating system and open the browser accordingly
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    open index.html
+    echo "Done!"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    if command -v xdg-open >/dev/null; then
+        xdg-open index.html
+        echo "Done!"
+    else
+        echo "No xdg-open available. Please open index.html manually."
+    fi
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" || "$OSTYPE" == "cygwin" ]]; then
+    # Windows
+    if command -v start >/dev/null; then
+        start index.html
+        echo "Done!"
+    else
+        echo "No start available. Please open index.html manually."
+    fi
+else
+    echo "Unsupported operating system. Please open index.html manually."
+fi
